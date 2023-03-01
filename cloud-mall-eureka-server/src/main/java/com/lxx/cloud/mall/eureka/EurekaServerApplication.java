c9dab6448f37292ae57269ac058e3eb0e45cacef
lin-xiuxian <2625453652@qq.com>
2023-03-01 23:35:07 +0800

    开发Eureka Server 模块

--- cloud-mall-eureka-server/src/main/java/com/lxx/cloud/mall/eureka/EurekaServerApplication.java
+++ cloud-mall-eureka-server/src/main/java/com/lxx/cloud/mall/eureka/EurekaServerApplication.java
@@ -1,9 +1,18 @@
 package com.lxx.cloud.mall.eureka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * @author 林修贤
  * @date 2023/3/1
- * @description
+ * @description eureka
  */
+@EnableEurekaServer
+@SpringBootApplication
 public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class);
+    }
 }
